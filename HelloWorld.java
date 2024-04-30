ae3b0364840fd0f0400b31a76c8ef9e30dcbfcff
royalai004 <156927439+royalai004@users.noreply.github.com>
2024-04-29 22:48:42 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,41 +1,53 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
+
 class HelloWorld {
     public static void main(String[] args) {
-        double height = 0;
-        double points_per_meter = 0;
-        double par = 0; //Naming variables
-        System.out.println("This program will give you a score and rating for your ski jump"); //Purpose of program
-        Scanner scan = new Scanner(System.in);
-        System.out.println("What was the jumper's speed at the end of the ramp in m/s?");
-        double speed = scan.nextInt(); //Storing user input
-        System.out.println("What type of hill was it? [Normal/Large]");
-        scan.nextLine();
-        String hill = scan.nextLine(); //Storing user input
 
-        if(hill.equals("normal")){
-            height = 46;
-            points_per_meter = 2;
-            par = 90;
+        System.out.println("Phone Bill Activity!");
+        DecimalFormat mon = new DecimalFormat("0.00");
+        Scanner input = new Scanner(System.in);
+        double monthly = 0;
+        double extra = 0;
+        double additional = 0;
+        double bill = 0;
+        String coupon = "No";
+
+        System.out.println("Hello, what package subscription do you have with us? ");
+        String packageType = input.next().toLowerCase();
+
+        System.out.println("How much data did you use this month? ");
+        double data = input.nextDouble();
+
+        if (packageType.equals("green")) {
+            monthly = 49.99;
+            extra = data - 2;
+            additional = 15;
+            System.out.println("Do you have a coupon (Yes or No)?");
+            coupon = input.next().toLowerCase();
+        } else if (packageType.equals("blue")) {
+            monthly = 70;
+            extra = data - 4;
+            additional = 10;
+        } else if (packageType.equals("purple")) {
+            monthly = 99.95;
+            extra = 0;
+            additional = 0;
         } else {
-            height = 70;
-            points_per_meter = 1.8;
-            par = 120;
-        } //Setting values according to hill type
-        double time = Math.sqrt((2 * height) / 9.8);
-        double distance = speed * time;
-        double points = (60 + (distance - par) * points_per_meter);
-        //Performing calculations based on input
-        if (points>=61){
-            System.out.println("Great job for doing better than par!");
+            System.out.println("Invalid package name. Can't calculate a price.");
+            packageType = "invalid";
         }
-        else if(points < 10){
-            System.out.println("What happened");
+
+        if (!packageType.equals("invalid")) {
+            if (extra > 0) {
+                bill = monthly + extra * additional;
+            } else {
+                bill = monthly;
+            }
+            if (coupon.toLowerCase().equals("yes") && bill >= 75) {
+                bill = bill - 20;
+            }
+            System.out.println("Your bill is $" + mon.format(bill)); // Output bill
         }
-        else{
-            System.out.println("Sorry, you didn't go very far.");
-        }
-        //Outputs different outcomes based on point result.
-        System.out.print("You had a distance of " + distance + " meters and earned " + points + " points. ");
-        //Outputs distance traveled and points
     }
 }
